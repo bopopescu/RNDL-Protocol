45945180f724ce54b62b7e6822279df3bceae544
Felix Holz <felix.holz@me.com>
2018-07-16 10:26:07 +0200

    transmitter and receiver classes

--- receiver.py
+++ receiver.py
@@ -3,61 +3,80 @@ import time
 import encoder
 from serialutil import *
 
-s = serial.Serial()
-s.port = "COM9"
-s.baudrate = 57600
-s.parity = serial.PARITY_NONE
-s.timeout = None
+class Receiver():
 
-s.open()
+    def __init__(self, callback):
+        self.sentence = []
 
-#test
-s.write(b"sys get ver\r\n")
-r = s.read_until(terminator=b"\n")
-print(clean_message(r))
+        self.s = serial.Serial()
+        self.s.port = "COM9"
+        self.s.baudrate = 57600
+        self.s.parity = serial.PARITY_NONE
+        self.s.timeout = None
+        self.s.open()
 
-write("mac pause", s)
-receive(s)
+        self.callback = callback
 
-sentence = []
+        self.init_lora()
+        self.receive_loop()
 
-while True:
-    write("radio rx 0", s)
-    receive(s) #ok
-
-    value = receive(s).decode()
-
-    value = value.replace("b", "")
-    value = value.replace("'", "")
-    value = value.replace("r", "")
-    value = value.replace("n", "")
-    value = value.replace("\\", "")
-    value = value.replace("  ", " ")
-
-    value = value.split(" ");
-    
-    if len(value) == 2:
-
-        value[1] = value[1].replace("\\r\\n", "")
-        valueint = 1
-        try:
-            ts = value[1]
-            valueint = 0 if ts[-4:-2] == "00" else 1
-            pass
-        except ValueError:
-            pass
         
-        value = encoder.decodehex(value[1])
-        print("Received packet (" + str(len(value)) + ")")
-        
-    else:
-        value = None
 
-    if value != None:
-        sentence.append(value)
+    def __del__(self):
+        self.s.close()
 
-        if valueint == 0:
-            print("".join(sentence))
-            sentence = []
-        
-s.close()
+    def init_lora(self):
+        self.s.write(b"sys get ver\r\n")
+        r = self.s.read_until(terminator=b"\n")
+        print(clean_message(r))
+
+        write("mac pause", self.s)
+        receive(self.s)
+
+
+    def receive_loop(self):
+        while True:
+            write("radio rx 0", self.s)
+            receive(self.s) #ok
+
+            value = receive(self.s).decode()
+
+            value = value.replace("b", "")
+            value = value.replace("'", "")
+            value = value.replace("r", "")
+            value = value.replace("n", "")
+            value = value.replace("\\", "")
+            value = value.replace("  ", " ")
+
+            value = value.split(" ")
+            
+            if len(value) == 2:
+
+                value[1] = value[1].replace("\\r\\n", "")
+                valueint = 1
+                try:
+                    ts = value[1]
+                    valueint = 0 if ts[-4:-2] == "00" else 1
+                    pass
+                except ValueError:
+                    pass
+                
+                value = encoder.decodehex(value[1])
+                print("Received packet (" + str(len(value)) + ")")
+                
+            else:
+                value = None
+
+            if value != None:
+                self.sentence.append(value)
+
+                if valueint == 0:
+                    received = "".join(self.sentence)
+                    self.callback(received)
+                    self.sentence = []
+
+def cb(msg):
+    print(msg)
+
+
+rec = Receiver(cb)
--- transmitter.py
+++ transmitter.py
@@ -2,31 +2,40 @@ import serial
 import encoder
 from serialutil import *
 
-s = serial.Serial()
-s.port = "COM8"
-s.baudrate = 57600
-s.parity = serial.PARITY_NONE
-s.open()
 
-#test
-s.write(b"sys get ver\r\n")
-r = s.read_until(terminator=b"\n")
-print(clean_message(r))
 
-write("mac pause", s)
-receive(s)
+class Transmitter():
 
-write("radio set pwr 14", s)
-receive(s)
+    def __init__(self):
+        self.s = serial.Serial()
+        self.s.port = "COM8"
+        self.s.baudrate = 57600
+        self.s.parity = serial.PARITY_NONE
+        self.s.open()
 
-while True:
-    sentence = input("sentence: ")
-    encoded = encoder.encodehex(sentence)
-    encoded[-1] += "00"
-    for single in encoded:
-        write("radio tx " + single, s)
-        print("sending " + single)
-        receive(s)
-        receive(s)
+    def __del__(self):
+        self.s.close()
 
-s.close()
+    def init_lora(self):
+        self.s.write(b"sys get ver\r\n")
+        r = self.s.read_until(terminator=b"\n")
+        print(clean_message(r))
+        
+        write("mac pause", self.s)
+        receive(self.s)
+
+        write("radio set pwr 14", self.s)
+        receive(self.s)
+
+    def transmit(self, msg):
+        encoded = encoder.encodehex(msg)
+        encoded[-1] += "00"
+        for single in encoded:
+            write("radio tx " + single, self.s)
+            print("sending " + single)
+            receive(self.s)
+            receive(self.s)
+
+
+t = Transmitter()
+t.transmit("findenig")
